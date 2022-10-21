0b7015cb30afe82a60dc1661baf880d78bc9c274
Hrutvik Kanabar <hrutvik@google.com>
2022-10-21 15:48:05 +0000

    prog: add a notion of a mutation "heatmap"
    In this context, a heatmap is a flexible mechanism to choose where to
    mutate some collection of bytes with some underlying probability
    distribution, which could be dependent on the bytes themselves.
    
    This commit defines the interface for heatmaps, implements a generic
    one, and adds a test for it.
    
    The expected usage of the heatmap inferface is:
      - Caller selects heatmap type based on program type (or other logic),
        and calls the appropriate `MakeXYZHeatmap`.
        Currently there is only one, but over time we will add more with differing
        probability distributions.
      - Caller queries the heatmap using `hm.ChooseLocation(r)` for random
        input `r`.
    
    The generic heatmap is based on the current compression for disk images. It
    avoids mutating within long runs of constant bytes by viewing the file
    as a series of 64-byte chunks, and ignoring chunks which consist of a
    single repeated byte.

---
+++ prog/heatmap_test.go
@@ -0,0 +1,118 @@
+// Copyright 2022 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package prog
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"testing"
+)
+
+type Region struct {
+	start int
+	end   int
+}
+
+func TestGenericHeatMap(t *testing.T) {
+	const tries = 10
+	iters := iterCount() / tries
+
+	r := rand.New(randSource(t))
+	for i := 0; i < iters; i++ {
+		data, regions := createData(r)
+		hm := MakeGenericHeatmap(data).(*GenericHeatmap)
+
+		for j := 0; j < tries; j++ {
+			index := hm.ChooseLocation(r)
+			if !checkIndex(index, len(data), regions) {
+				t.Fatalf("selected index %d does not fall in a region\n", index)
+				debugPrintGenericHeatmap(data, regions, hm)
+			}
+		}
+	}
+}
+
+// Create a byte slice which is mostly a single byte. Return the data and the regions we want the heatmap to select.
+func createData(r *rand.Rand) ([]byte, []Region) {
+	// Initialise slice up to 1 MB in length.
+	len := r.Intn(1<<20) + 1
+	data := make([]byte, len)
+
+	// Fill slice with constant byte.
+	constByte := byte(r.Intn(256))
+	for i := range data {
+		data[i] = constByte
+	}
+
+	// Randomly select "interesting" regions. We don't care about overlapping regions.
+	numRegions := r.Intn(10)
+	var regions []Region
+	for i := 0; i < numRegions; i++ {
+		start := r.Intn(len)
+		length := r.Intn((len/100)+1) + (granularity / 5) // Ensure non-empty.
+		if start+length > len {
+			continue
+		}
+		regions = append(regions, Region{start, start + length})
+	}
+
+	// Fill "interesting" regions with random data.
+	for _, region := range regions {
+		r.Read(data[region.start:region.end])
+	}
+	return data, regions
+}
+
+// Check an index is within some (aligned) region.
+func checkIndex(index, maxIndex int, regions []Region) bool {
+	if index < 0 || index >= maxIndex {
+		return false
+	}
+
+	if len(regions) == 0 {
+		// Index is chosen uniformly up to maxIndex.
+		return true
+	}
+	for _, region := range regions {
+		start, end := roundDown(region.start), roundDown(region.end)+granularity
+		if start <= index && index < end {
+			return true
+		}
+	}
+	return false
+}
+
+func roundDown(i int) int {
+	return (i / granularity) * granularity
+}
+
+func debugPrintGenericHeatmap(data []byte, regions []Region, hm *GenericHeatmap) {
+	// Print data.
+	fmt.Printf("data: len = %d\n", len(data))
+	for j := 0; j < len(data); j += granularity {
+		end := j + granularity
+		if end > len(data) {
+			end = len(data)
+		}
+		fmt.Printf("%8d: %x\n", j*granularity, data[j:end])
+	}
+	fmt.Println()
+
+	// Print selected regions in data.
+	sort.Slice(regions, func(i, j int) bool {
+		return regions[i].start < regions[j].start
+	})
+	for j, region := range regions {
+		fmt.Printf("region  %4d: %8v - %8v\n", j, region.start, region.end)
+	}
+	fmt.Println()
+
+	// Print heatmap.
+	fmt.Printf("Heatmap (total segment length %d, total length %d)\n", hm.length, hm.rawLength)
+	for j, seg := range hm.segments {
+		fmt.Printf("segment %4d: %8v - %8v\n", j, seg.offset, seg.offset+seg.length)
+	}
+	fmt.Printf("\n\n\n")
+}
